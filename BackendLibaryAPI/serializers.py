3514095c40c82216fa154fffaf7f090ef532d63f
Jakub Pawi <kubakson25@wp.pl>
2023-01-14 10:39:26 +0100

    added books,autgor,categories models

--- BackendLibaryAPI/serializers.py
+++ BackendLibaryAPI/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 
-from .models import BookModel
+from .models import Book,Author,Category
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
-    class Book:
-        model = BookModel
-        fields = ('title', 'author')
+    
+    class Meta:
+        model = Book
+        fields = ('id','title','isbn','date_of_publication','quantity','book_description','author_id','category_id')
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+     
+    class Meta:
+        model = Author
+        fields = ('id','name', 'surname','birthday','nationality','author_description')
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+     
+    class Meta:
+        model = Category
+        fields = ('id','name', 'category_description')
+    
